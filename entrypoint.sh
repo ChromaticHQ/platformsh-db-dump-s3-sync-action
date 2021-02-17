bdaeec02f65a6b5f0622461004145a45aa000f3e
Märt Matsoo <mart@chromatichq.com>
2021-02-17 13:52:22 +0200

    Add platformsh_relationship variable support

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,16 @@ platform --version
 sed -i 's/#   StrictHostKeyChecking ask.*/StrictHostKeyChecking accept-new/' /etc/ssh/ssh_config
 FILENAME="${INPUT_DB_DUMP_FILENAME_BASE}-$(date +%F-%T)"
 
-platform db:dump -v --yes --project "$INPUT_PLATFORMSH_PROJECT" --environment "$INPUT_PLATFORMSH_ENVIRONMENT" --gzip -f "$FILENAME".sql.gz
+# Use an array for extra arguments. Easy to add more in the future.
+args=()
+# Check if the optional relationship value exists.
+if [ -z ${INPUT_PLATFORMSH_RELATIONSHIP} ]
+then
+  echo "No relationship variable provided so no relationship parameter added to platform db:dump."
+else
+  args+=( "--relationship $INPUT_PLATFORMSH_RELATIONSHIP" )
+fi
+
+platform db:dump -v --yes --project "$INPUT_PLATFORMSH_PROJECT" --environment "$INPUT_PLATFORMSH_ENVIRONMENT" "${args[@]}" --gzip -f "$FILENAME".sql.gz
 
 aws s3 cp "$FILENAME".sql.gz s3://"$INPUT_AWS_S3_BUCKET"
