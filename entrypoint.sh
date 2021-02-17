9dceb5fdb2457b8cfb730e701d33d9d32501f6df
Märt Matsoo <mart@chromatichq.com>
2021-02-17 16:50:12 +0200

    Give up on using array syntax

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,15 +8,14 @@ sed -i 's/#   StrictHostKeyChecking ask.*/StrictHostKeyChecking accept-new/' /et
 FILENAME="${INPUT_DB_DUMP_FILENAME_BASE}-$(date +%F-%T)"
 
 # Check if the optional relationship value exists.
+RELATIONSHIP=""
 if [ -z "${INPUT_PLATFORMSH_RELATIONSHIP}" ]
 then
   echo "No relationship variable provided so no relationship parameter added to platform db:dump."
 else
-  # Manually add to args variable which can then hold many parameters.
-  # Subsequent parameters can be added in the same way, increasing the index.
-  args[0]="--relationship $INPUT_PLATFORMSH_RELATIONSHIP"
+  RELATIONSHIP="--relationship $INPUT_PLATFORMSH_RELATIONSHIP"
 fi
 
-platform db:dump -v --yes --project "$INPUT_PLATFORMSH_PROJECT" --environment "$INPUT_PLATFORMSH_ENVIRONMENT" "${args[@]}" --gzip -f "$FILENAME".sql.gz
+platform db:dump -v --yes --project "$INPUT_PLATFORMSH_PROJECT" --environment "$INPUT_PLATFORMSH_ENVIRONMENT" "$RELATIONSHIP" --gzip -f "$FILENAME".sql.gz
 
 aws s3 cp "$FILENAME".sql.gz s3://"$INPUT_AWS_S3_BUCKET"
