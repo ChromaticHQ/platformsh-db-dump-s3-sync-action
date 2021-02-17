1729d7ea44790a929c2a876476a0b7fe612ec5f7
Märt Matsoo <mart@chromatichq.com>
2021-02-17 16:34:45 +0200

    Remove array instantiation which is not supported in POSIX

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,14 +7,13 @@ platform --version
 sed -i 's/#   StrictHostKeyChecking ask.*/StrictHostKeyChecking accept-new/' /etc/ssh/ssh_config
 FILENAME="${INPUT_DB_DUMP_FILENAME_BASE}-$(date +%F-%T)"
 
-# Use an array for extra arguments. Easy to add more in the future.
-args=()
 # Check if the optional relationship value exists.
-if [ -z ${INPUT_PLATFORMSH_RELATIONSHIP} ]
+if [ -z "${INPUT_PLATFORMSH_RELATIONSHIP}" ]
 then
   echo "No relationship variable provided so no relationship parameter added to platform db:dump."
 else
-  # Manually add to index zero for POSIX, which rejected += syntax.
+  # Manually add to args variable which can then hold many parameters.
+  # Subsequent parameters can be added in the same way, increasing the index.
   args[0]="--relationship $INPUT_PLATFORMSH_RELATIONSHIP"
 fi
 
