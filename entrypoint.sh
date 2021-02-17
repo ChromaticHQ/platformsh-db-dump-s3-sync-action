1e9c490f7f08c9547b43a77769ac4578e363c90a
Märt Matsoo <mart@chromatichq.com>
2021-02-17 18:36:50 +0200

    Add platformsh_relationship variable support (#3)
    * Add platformsh_relationship variable support
    
    Co-authored-by: Mark Dorison <mark@chromatichq.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,15 @@ platform --version
 sed -i 's/#   StrictHostKeyChecking ask.*/StrictHostKeyChecking accept-new/' /etc/ssh/ssh_config
 FILENAME="${INPUT_DB_DUMP_FILENAME_BASE}-$(date +%F-%T)"
 
-platform db:dump -v --yes --project "$INPUT_PLATFORMSH_PROJECT" --environment "$INPUT_PLATFORMSH_ENVIRONMENT" --gzip -f "$FILENAME".sql.gz
+# Check if the optional relationship value exists.
+RELATIONSHIP=""
+if [ -z "${INPUT_PLATFORMSH_RELATIONSHIP}" ]
+then
+  echo "No relationship variable provided so no relationship parameter added to platform db:dump."
+else
+  RELATIONSHIP="--relationship $INPUT_PLATFORMSH_RELATIONSHIP"
+fi
+
+platform db:dump -v --yes --project "$INPUT_PLATFORMSH_PROJECT" --environment "$INPUT_PLATFORMSH_ENVIRONMENT" "$RELATIONSHIP" --gzip -f "$FILENAME".sql.gz
 
 aws s3 cp "$FILENAME".sql.gz s3://"$INPUT_AWS_S3_BUCKET"
