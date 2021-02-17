a1ee355dd7927345361372cec11eab49a6dea61f
Märt Matsoo <mart@chromatichq.com>
2021-02-17 21:57:24 +0200

    Put platform db:dump command into conditional (#4)

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,14 +8,13 @@ sed -i 's/#   StrictHostKeyChecking ask.*/StrictHostKeyChecking accept-new/' /et
 FILENAME="${INPUT_DB_DUMP_FILENAME_BASE}-$(date +%F-%T)"
 
 # Check if the optional relationship value exists.
-RELATIONSHIP=""
 if [ -z "${INPUT_PLATFORMSH_RELATIONSHIP}" ]
 then
-  echo "No relationship variable provided so no relationship parameter added to platform db:dump."
+  # Run command without --relationship parameter.
+  platform db:dump -v --yes --project "$INPUT_PLATFORMSH_PROJECT" --environment "$INPUT_PLATFORMSH_ENVIRONMENT" --gzip -f "$FILENAME".sql.gz
 else
-  RELATIONSHIP="--relationship $INPUT_PLATFORMSH_RELATIONSHIP"
+  # Run command with --relationship parameter.
+  platform db:dump -v --yes --project "$INPUT_PLATFORMSH_PROJECT" --environment "$INPUT_PLATFORMSH_ENVIRONMENT" --relationship "$INPUT_PLATFORMSH_RELATIONSHIP" --gzip -f "$FILENAME".sql.gz
 fi
-
-platform db:dump -v --yes --project "$INPUT_PLATFORMSH_PROJECT" --environment "$INPUT_PLATFORMSH_ENVIRONMENT" "$RELATIONSHIP" --gzip -f "$FILENAME".sql.gz
 
 aws s3 cp "$FILENAME".sql.gz s3://"$INPUT_AWS_S3_BUCKET"
