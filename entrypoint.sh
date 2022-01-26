2dd1faed51099c131bdbcebe283153f481497fc4
Märt Matsoo <mart@chromatichq.com>
2022-01-26 21:17:44 +0200

    Remove double quotes

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,10 +20,10 @@ done
 if [ -z "${INPUT_PLATFORMSH_RELATIONSHIP}" ]
 then
   # Run command without --relationship parameter.
-  platform db:dump -v --yes --project "$INPUT_PLATFORMSH_PROJECT" --environment "$INPUT_PLATFORMSH_ENVIRONMENT" "${DUMP_ONLY_THESE_TABLES}" --gzip -f "$FILENAME".sql.gz
+  platform db:dump -v --yes --project "$INPUT_PLATFORMSH_PROJECT" --environment "$INPUT_PLATFORMSH_ENVIRONMENT" ${DUMP_ONLY_THESE_TABLES} --gzip -f "$FILENAME".sql.gz
 else
   # Run command with --relationship parameter.
-  platform db:dump -v --yes --project "$INPUT_PLATFORMSH_PROJECT" --environment "$INPUT_PLATFORMSH_ENVIRONMENT" --relationship "$INPUT_PLATFORMSH_RELATIONSHIP" "${DUMP_ONLY_THESE_TABLES}" --gzip -f "$FILENAME".sql.gz
+  platform db:dump -v --yes --project "$INPUT_PLATFORMSH_PROJECT" --environment "$INPUT_PLATFORMSH_ENVIRONMENT" --relationship "$INPUT_PLATFORMSH_RELATIONSHIP" ${DUMP_ONLY_THESE_TABLES} --gzip -f "$FILENAME".sql.gz
 fi
 
 aws s3 cp "$FILENAME".sql.gz s3://"$INPUT_AWS_S3_BUCKET"
